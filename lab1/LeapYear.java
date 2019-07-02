507b34170bf4f785427d5f4ed204e2914eb1cd69
zhihua0322 <sunzhihua980322@gmail.com>
2019-07-02 18:02:04 +0800

    lab1 finished

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,14 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author ZHIHUA0322
  */
 public class LeapYear {
+    public static boolean isLeapYear(int year) {
+        if ((year % 400 == 0) || (year%4==0 && year%100!=0) ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
