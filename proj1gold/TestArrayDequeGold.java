77801a9512c546368d9a0557f0bceca4f095b5ac
zhihua0322 <sunzhihua980322@gmail.com>
2019-07-04 21:29:44 +0800

    proj1gold finished

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class TestArrayDequeGold {
+    private StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+    private ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+    private int loopVal = 10;
+
+    @Test
+    public void TestDeque1() {
+        for (int i = 0; i <= 10; i++) {
+            int first = StdRandom.uniform(10);
+            student.addFirst(first);
+            solution.addFirst(first);
+        }
+        for (int i = 0; i <= 10; i++) {
+            assertEquals(student.removeFirst(), solution.removeFirst());
+        }
+        for (int i = 0; i <= 10; i++) {
+            int last = StdRandom.uniform(11, 20);
+            student.addLast(last);
+            solution.addLast(last);
+        }
+        for (int i = 0; i <= 10; i++) {
+            assertEquals(student.removeLast(), solution.removeLast());
+        }
+    }
+
+    @Test
+    public void TestDeque2() {
+        String msg = "\n";
+        for (int i = 0; i <= 10; i++) {
+            int first = StdRandom.uniform(10);
+            student.addFirst(first);
+            solution.addFirst(first);
+            msg = msg + "addFirst(" + first + ")\n";
+        }
+        for (int i = 0; i <= 10; i++) {
+            msg = msg + "removeFirst()\n";
+            assertEquals(msg, student.removeFirst(), solution.removeFirst());
+        }
+        for (int i = 0; i <= 10; i++) {
+            int last = StdRandom.uniform(11, 20);
+            student.addLast(last);
+            solution.addLast(last);
+            msg = msg + "addLast(" + last + ")\n";
+        }
+        for (int i = 0; i <= 10; i++) {
+            msg = msg + "removeLast()\n";
+            assertEquals(msg, student.removeLast(), solution.removeLast());
+        }
+    }
 }
