9afb31c9caf3de71baf053c276c6a77663f71ee9
zhihua0322 <sunzhihua980322@gmail.com>
2019-07-21 15:43:16 +0800

    lab8 finished

--- lab8/MyHashMap.java
+++ lab8/MyHashMap.java
@@ -1,186 +1,238 @@
 import java.util.LinkedList;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.ArrayList;
 import java.util.HashSet;
 
 public class MyHashMap<K, V> implements Map61B<K, V> {
-    private double loadFactor = 0.75;
-    private int size = 0;
-    private int capacity = 5;
-    private HashSet<K> keySet = new HashSet<K>();
-    private Entry<K, V>[] table;
-    private int threshhold;
+    private static final int INIT_CAPACITY = 16;
+    private static final double INIT_LOADFACTOR = .75;
+    private int n;                          // number of key-value pairs
+    private int size;                       // hash table size
+    private double loadFactor;
+    private LinkedList<Entry<K, V>>[] bins;
 
-    public MyHashMap(){
-        threshhold = (int)(loadFactor * capacity);
-        table = new Entry[capacity];
-    }
-    public MyHashMap(int initialCapacity){
-        capacity = initialCapacity;
-        threshhold = (int)(loadFactor * capacity);
-        table = new Entry[capacity];
-    }
-    public MyHashMap(int initialCapacity, float loadFactor){
-        capacity = initialCapacity;
-        this.loadFactor =  loadFactor;
-        threshhold = (int)(loadFactor * capacity);
-        table = (Entry<K,V>[])new Object[capacity];
+
+    public MyHashMap() {
+        this(INIT_CAPACITY, INIT_LOADFACTOR);
     }
 
-    private static class Entry<K,V>{
+    public MyHashMap(int initialSize) {
+        this(initialSize, INIT_LOADFACTOR);
+    }
+
+    public MyHashMap(int initialSize, double loadFactor) {
+        size = initialSize;
+        this.loadFactor = loadFactor;
+        bins = (LinkedList<Entry<K, V>>[]) new LinkedList[size];
+    }
+
+    private class Entry<K, V> {
         private K key;
-        private V val;
-        private Entry<K,V> next;
-        private boolean deleted;
+        private V value;
 
-        public Entry(K key, V value, Entry<K,V> next){
+        public Entry(K key, V value) {
             this.key = key;
-            this.val = value;
-            this.next = next;
-            deleted = false;
+            this.value = value;
         }
 
-        public Entry(K key, V value, Entry<K,V> next, boolean deleted){
-            this.key = key;
-            this.val = value;
-            this.next = next;
-            this.deleted = deleted;
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public V setValue (V x) {
+            V old = value;
+            this.value = x;
+            return old;
+        }
+        @Override
+        public String toString() {
+            return "<" + key + "," + value + ">";
         }
     }
 
+    private int hash(K key, int size) {
+        return (key.hashCode() & 0x7fffffff) % size;
+    }
+
+
+    /* Removes all of the mappings from this map. */
     @Override
     public void clear() {
-        size = 0;
-        threshhold =(int) (capacity * loadFactor);
-        table = new Entry[capacity];
+        n = 0;
+        bins = (LinkedList<Entry<K, V>>[]) new LinkedList[size];
     }
 
+    /* Returns true if this map contains a mapping for the specified key. */
     @Override
-    public boolean containsKey(K key){
+    public boolean containsKey(K key) {
+        if (key == null) {
+            throw new NullPointerException();
+        }
         return get(key) != null;
     }
 
+    /* Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     */
     @Override
-    public V get(K key){
-        int hashcode = hash(key.hashCode());
-        int index = hashcode % capacity;
-        for(Entry<K, V> e = table[index]; e!= null; e=e.next){
-            if(e.key.equals(key) && !e.deleted){
-                return e.val;
-            }
+    public V get(K key) {
+        if (key == null) {
+            throw new NullPointerException("argument to get() is null");
         }
-        return null;
+        int i = hash(key, size);
+        Entry<K, V> e = find(key, bins[i]);
+        return (e == null) ? null : e.value;
     }
 
-    @Override
-    public int size(){
-        return size;
-    }
-
-    @Override
-    public void put(K key, V value){
-        int hashcode = hash(key.hashCode());
-        int index = hashcode % capacity;
-        for(Entry<K, V> e = table[index]; e!= null; e=e.next){
-            if(e.key.equals(key)&&!e.deleted){
-                e.val = value;
-                return;
-            }
-        }
-        table[index] = new Entry<K, V>(key, value, table[index]);
-        size++;
-        if(size > threshhold){
-            rehash();
-        }
-    }
-
-    public void rehash(){
-        int oldCapacity = capacity;
-        capacity = capacity*2+1;
-        Entry[] oldTable = table;
-        clear();
-        for(int i = 0; i < oldCapacity; ++i){
-            for(Entry<K,V> e = oldTable[i];e != null;e = e.next){
-                if(e.deleted) continue;
-                int hashcode = hash(e.key.hashCode());
-                int index = hashcode % capacity;
-                table[index] = new Entry<K, V>(e.key, e.val, table[index]);
-                size++;
-            }
-        }
-
-    }
-
-    @Override
-    public V remove(K key){
-        int hashcode = hash(key.hashCode());
-        int index = hashcode % capacity;
-        Entry<K,V> f = table[index];
-        if(f==null){
+    /* The Entry in the list BIN whose key is KEY, or null if none. */
+    private Entry<K, V> find(K key, LinkedList<Entry<K, V>> bin) {
+        if (bin == null) {
             return null;
         }
-        for(Entry<K,V> e = table[index];e != null;e = e.next){
-            if(e.key.equals(key) && e.deleted == false){
-                e.deleted=true;
-                size--;
-                return e.val;
+        for (Entry<K, V> e : bin) {
+            if (key.equals(e.key)) {
+                return e;
             }
         }
         return null;
     }
 
-    @Override
-    public V remove(K key, V value){
-        int hashcode = hash(key.hashCode());
-        int index = hashcode % capacity;
-        Entry<K,V> f = table[index];
-        if(f==null){
-            return null;
-        }
-        for(Entry<K,V> e = table[index];e != null;e = e.next){
-            if(e.key.equals(key) && e.val == value && e.deleted == false){
-                e.deleted = true;
-                size--;
-                return e.val;
+    public Set<Entry<K, V>> entrySet() {
+        Set<Entry<K, V>> set = new HashSet<>();
+
+        for (int i = 0; i < size; i++) {
+            if (bins[i] != null) {
+                LinkedList<Entry<K, V>> bin = bins[i];
+                for (Entry<K, V> entry : bin) {
+                    set.add(entry);
+                }
             }
+        }
+        return set;
+    }
+
+    private void resize() {
+        int newsize = size * 2;
+        MyHashMap<K, V> newMap =  new MyHashMap<>(newsize, loadFactor);
+        for (Entry<K, V> e : entrySet()) {
+            newMap.put(e.key, e.value);
+        }
+        this.bins = newMap.bins;
+        this.n = newMap.n;
+        this.size = newMap.size;
+    }
+
+    private LinkedList<Entry<K, V>> getEntryList(K key) {
+        int i = hash(key, size);
+        LinkedList<Entry<K, V>> entryList = bins[i];
+        if (entryList == null) {
+            entryList = new LinkedList<>();
+            bins[i] = entryList;
+        }
+        return entryList;
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    @Override
+    public int size() {
+        return n;
+    }
+
+    /* Associates the specified value with the specified key in this map. */
+    @Override
+    public void put(K key, V value) {
+        if (key == null || value == null) {
+            throw new NullPointerException("argument to put() is null");
+        }
+        if(get(key) == null) {
+            int i = hash(key, size);
+            LinkedList<Entry<K, V>> bin = getEntryList(key);
+            for (Entry<K, V> entry : bin) {
+                if (key.equals(entry.key)) {
+                    entry.value = value;
+                }
+            }
+            Entry<K, V> e = new Entry<>(key, value);
+            bin.add(e);
+            n += 1;
+            bins[i] = bin;
+            if ((double) n / size > loadFactor) {
+                resize();
+            }
+        } else {
+            int i = hash(key, size);
+            Entry<K, V> e = find(key, bins[i]);
+            e.setValue((V)value);
+        }
+    }
+
+    /* Returns a Set view of the keys contained in this map. */
+    @Override
+    public Set<K> keySet() {
+        Set<K> keys = new HashSet<>();
+        for (int i = 0; i < size; i++) {
+            if (bins[i] != null) {
+                LinkedList<Entry<K, V>> bin = bins[i];
+                for (Entry<K, V> entry : bin) {
+                    keys.add(entry.key);
+                }
+            }
+        }
+        return keys.size() == 0 ? null : keys;
+    }
+
+    /* Removes the mapping for the specified key from this map if present.
+     * Not required for Lab 8. If you don't implement this, throw an
+     * UnsupportedOperationException. */
+    @Override
+    public V remove(K key) {
+        if (key == null) {
+            throw new NullPointerException("argument to remove() is null");
+        }
+        int i = hash(key, size);
+        Entry<K, V> e = find(key, bins[i]);
+        V oldValue = e.value;
+        e.value = null;
+        return oldValue;
+    }
+
+    /* Removes the entry for the specified key only if it is currently mapped to
+     * the specified value. Not required for Lab 8. If you don't implement this,
+     * throw an UnsupportedOperationException.*/
+    @Override
+    public V remove(K key, V value) {
+        if (key == null) {
+            throw new NullPointerException("argument to remove() is null");
+        }
+        int i = hash(key, size);
+        Entry<K, V> e = find(key, bins[i]);
+        if (e.value.equals(value)) {
+            V oldValue = e.value;
+            LinkedList<Entry<K, V>> bin = bins[i];
+            bin.remove(e);
+            n -= 1;
+            bins[i] = bin;
+            return oldValue;
         }
         return null;
     }
 
-    @Override
-    public Set<K> keySet(){
-        return keySet;
+    public Iterator iterator() {
+        Set keys = keySet();
+        return keys.iterator();
     }
 
-    /* this function is from java library code
-        http://stackoverflow.com/questions/9364134/what-hashing-function-does-java-use-to-implement-hashtable-class
-    */
-    private int hash(int h){
-        h ^= (h >>> 20) ^ (h >>> 12);
-        return h ^ (h >>> 7) ^ (h >>> 4);
-    }
-
-    @Override
-    public Iterator<K,V> iterator() {
-        return new HashMapIterator();
-    }
-
-    private class HashMapIterator implements Iterator<K> {
-        private int wizPos;
-
-        public HashMapIterator() {
-            wizPos = 0;
+    public String toString() {
+        String output = "";
+        for (int i = 0; i < bins.length; i += 1) {
+            String contents = bins[i] == null ? "null" : ((LinkedList) bins[i]).toString();
+            output += "[" + i + "] = " + contents + "\n";
         }
-
-        public boolean hasNext() {
-            return wizPos < size;
-        }
-
-        public K next() {
-            K returnItem = [wizPos];
-            wizPos += 1;
-            return returnItem;
-        }
+        return output;
     }
 }
